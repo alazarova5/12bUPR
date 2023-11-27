3252c26341e2653a6d1e2b4fcdfea40a9e7675b9
Your Name <you@example.com>
2023-11-27 10:28:27 +0200

    admin and client

---
+++ 03.DogsApp-Services - Copy/DogsApp/DogsApp.Infrastructure/Data/Infrastructure/ApplicationBuilderExtension.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using DogsApp.Infrastructure.Data.Domain;
+
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DogsApp.Infrastructure.Data.Infrastructure
+{
+    public static class ApplicationBuilderExtension
+    {
+        public static async Task<IApplicationBuilder> PrepareDatabase(this IApplicationBuilder app)
+        {
+            using var serviceScope = app.ApplicationServices.CreateScope();
+            var services = serviceScope.ServiceProvider;
+
+            await RoleSeeder(services);
+            await SeedAdimistrator(services);
+
+            var data = serviceScope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+            SeedBreeds(data);
+            return app;
+        }
+
+
+        public static void SeedBreeds(ApplicationDbContext data)
+        {
+            if (data.Breeds.Any())
+            {
+                return;
+            }
+            data.Breeds.AddRange(new[]
+            {
+                new Breed {Name = "Husky"},
+                new Breed {Name = "Pinscher"},
+                new Breed {Name = "Cocer spaniol"},
+                new Breed {Name = "Labrador"},
+                new Breed {Name = "Doberman"},
+            });
+            data.SaveChanges();
+        }
+
+
+        private static async Task RoleSeeder(IServiceProvider servicepProvider)
+        {
+            var roleManager = servicepProvider.GetRequiredService<RoleManager<IdentityRole>>();
+            string[] roleNames = { "Administrator", "Client" };
+            IdentityResult roleResult;
+            foreach (var role in roleNames)
+            {
+                var roleExist = await roleManager.RoleExistsAsync(role);
+                if (!roleExist)
+                {
+                    roleResult = await roleManager.CreateAsync(new IdentityRole(role));
+                }
+            }
+        }
+
+
+        private static async Task SeedAdimistrator(IServiceProvider serviceProvider)
+        {
+            var userManager = serviceProvider.GetRequiredService<UserManager<ApplicationUser>>();
+            if (await userManager.FindByNameAsync("admin") == null)
+            {
+                ApplicationUser user = new ApplicationUser();
+                user.FirstNane = "admin";
+                user.LastNane = "admin";
+                user.PhoneNumber = "0895519837";
+                user.UserName = "admin";
+                user.Email ="admin@admin.com";
+
+                var result = await userManager.CreateAsync(user, "111111");
+                if (result.Succeeded)
+                {
+                    userManager.AddToRoleAsync(user, "Administrator").Wait();
+                }
+            
+            }
+        }
+    }
+}
