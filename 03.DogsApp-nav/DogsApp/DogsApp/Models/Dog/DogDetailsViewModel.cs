1fcd985a704f1ab40246d45889a35cafe7f149d2
Your Name <you@example.com>
2023-11-30 11:12:09 +0200

    nav

---
+++ 03.DogsApp-nav/DogsApp/DogsApp/Models/Dog/DogDetailsViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace DogsApp.Models.Dog
+{
+    public class DogDetailsViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public int Age { get; set; }
+        public string BreedName { get; set; } = null!;
+        public string? Picture { get; set; }
+        public string FullName { get; set; } = null!;
+    }
+}
